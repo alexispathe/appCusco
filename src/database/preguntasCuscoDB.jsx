d61f04e7f99f8f21f69fd7e4e0b321326c376682
Alexis Pathe <alexispatheguillen@gmail.com>
2023-11-23 11:05:34 -0600

    Inicia el proyecto CUSCO

---
+++ src/database/preguntasCuscoDB.jsx
@@ -0,0 +1,21 @@
+const caracteristicasSociodemograficasDB=[
+    {
+        title: '¿Cuál es su sexo?',
+        options: [
+            {
+                option: 'Hombre',
+                optionID: 1
+            },
+            {
+                option: 'Mujer',
+                optionID: 2
+            }
+        ],
+        sociodemograficaID: 'cual-es-su-sexo-123'
+    },
+    {
+        title: '¿Cuál es su estado civil?',
+        options: ['Soltero(a)','Casado(a)','Viudo(a)', 'Divorciado(a)','Unión libre'],
+    },
+    
+]
