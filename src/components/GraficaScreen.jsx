d92f5e3e7971b88a8d2a1764f5f2c602da202b61
Alexis Pathe <alexispatheguillen@gmail.com>
2023-12-11 11:42:42 -0600

    Se implementa la ultima configuracion para las graficas con react-native-charts-wrapper

--- src/components/GraficaScreen.jsx
+++ src/components/GraficaScreen.jsx
@@ -1,18 +1,24 @@
 import {handleGetData} from '../assets/questionnaireFunctions';
 import {useEffect, useState} from 'react';
-import {ScrollView, View, Dimensions, Text, processColor} from 'react-native';
+import {
+  ScrollView,
+  View,
+  Dimensions,
+  Text,
+  processColor,
+  ActivityIndicator,
+} from 'react-native';
 import {BarChart} from 'react-native-charts-wrapper';
 import {prueba as preguntas} from '../database/preguntasCuscoDB';
 
 export const GraficaScreen = () => {
   const [dataGraficas, setDataGraficas] = useState([]);
-  const [valuesArray, setValuesArray] = useState();
-
+  const [isLoading, setIsLoading] = useState(true); // Estado para controlar la visualización del indicador de carga
   useEffect(() => {
     getData();
-    
   }, []);
   const getData = async () => {
+    setIsLoading(true); // Mostrar el indicador de carga al obtener los datos
     const dataCloudFirestore = await handleGetData();
     const dataSurvey = dataCloudFirestore.filter(
       data => data.dataType === 'written',
@@ -24,19 +30,30 @@ export const GraficaScreen = () => {
     procesarRespuestas(datos);
   };
 
-  const procesarRespuestas = async (respuestas) => {
+  const procesarRespuestas = async respuestas => {
     if (respuestas && respuestas.length >= 1) {
-      console.log(respuestas)
       const preguntasAProcesar = preguntas[0].questions; // Obtén las preguntas del arreglo preguntas
-
       const dataProcesada = [];
 
       for (const pregunta of preguntasAProcesar) {
         if (pregunta.questionType === 'numberInput') {
-          const respuestasPregunta = {};
-          const intervalos = [0, 10, 20, 30, 40]; // Define los intervalos según tu lógica
-          // ... Realiza la lógica para contar la cantidad de personas en cada intervalo
-          // Agrega esta información al objeto respuestasPregunta
+            const respuestasPregunta = {};
+            const intervalos = [0, 10, 20, 30, 40]; // Define los intervalos según tu lógica
+    
+            respuestas.forEach(respuesta => {
+              const valorRespuesta = respuesta[pregunta.questionID];
+              for (let i = 0; i < intervalos.length - 1; i++) {
+                if (valorRespuesta >= intervalos[i] && valorRespuesta < intervalos[i + 1]) {
+                  const rangoIntervalo = `${intervalos[i]}-${intervalos[i + 1]}`;
+                  if (!respuestasPregunta.hasOwnProperty(rangoIntervalo)) {
+                    respuestasPregunta[rangoIntervalo] = 0;
+                  }
+                  respuestasPregunta[rangoIntervalo]++;
+                  break;
+                }
+              
+            }
+          });
           dataProcesada.push({
             pregunta: pregunta.title,
             respuestas: respuestasPregunta,
@@ -67,6 +84,7 @@ export const GraficaScreen = () => {
       }
 
       setDataGraficas(dataProcesada);
+      setIsLoading(false);
     }
   };
 
@@ -94,28 +112,31 @@ export const GraficaScreen = () => {
   return (
     <ScrollView style={{flex: 1}}>
       <View style={{flex: 1}}>
-        {dataGraficas.map((pregunta, index) => (
-              <View key={index} style={{height: 300}}>
-                <Text
-                  style={{
-                    textAlign: 'center',
-                    color: 'black',
-                    fontWeight: 'bold',
-                  }}>
-                  {pregunta.pregunta}
-                </Text>
-                <BarChart
-                  style={{flex: 1}}
-                  data={generarDatosGrafica(index)}
-                  xAxis={{drawLabels: true, position: 'BOTTOM'}}
-                  yAxisRight={{drawLabels: false}}
-                  yAxisLeft={{drawLabels: true}}
-                  chartDescription={{text: ''}}
-                  legend={{enabled: true}}
-                />
-              </View>
-            ))
-          }
+        {isLoading ? (
+          <ActivityIndicator size="large" color="#2979FF" />
+        ) : (
+          dataGraficas.map((pregunta, index) => (
+            <View key={index} style={{height: 300}}>
+              <Text
+                style={{
+                  textAlign: 'center',
+                  color: 'black',
+                  fontWeight: 'bold',
+                }}>
+                {pregunta.pregunta}
+              </Text>
+              <BarChart
+                style={{flex: 1}}
+                data={generarDatosGrafica(index)}
+                xAxis={{drawLabels: true, position: 'BOTTOM'}}
+                yAxisRight={{drawLabels: false}}
+                yAxisLeft={{drawLabels: true}}
+                chartDescription={{text: ''}}
+                legend={{enabled: true}}
+              />
+            </View>
+          ))
+        )}
       </View>
     </ScrollView>
   );
