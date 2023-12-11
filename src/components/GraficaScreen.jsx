696bb49ee3f1994fbadfbf8a565e76b86d5cadbc
Alexis Pathe <alexispatheguillen@gmail.com>
2023-12-11 12:17:02 -0600

    Se implementan las graficas

--- src/components/GraficaScreen.jsx
+++ src/components/GraficaScreen.jsx
@@ -1,81 +1,72 @@
-import {handleGetData} from '../assets/questionnaireFunctions';
-import {useEffect, useState} from 'react';
-import {
-  ScrollView,
-  View,
-  Dimensions,
-  Text,
-  processColor,
-  ActivityIndicator,
-} from 'react-native';
-import {BarChart} from 'react-native-charts-wrapper';
-import {prueba as preguntas} from '../database/preguntasCuscoDB';
+import { useEffect, useState } from 'react';
+import { ScrollView, View, Text, ActivityIndicator,Dimensions } from 'react-native';
+import { BarChart } from 'react-native-chart-kit';
+import { handleGetData } from '../assets/questionnaireFunctions';
+import { prueba as preguntas } from '../database/preguntasCuscoDB';
 
 export const GraficaScreen = () => {
   const [dataGraficas, setDataGraficas] = useState([]);
-  const [isLoading, setIsLoading] = useState(true); // Estado para controlar la visualización del indicador de carga
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     getData();
   }, []);
+
   const getData = async () => {
-    setIsLoading(true); // Mostrar el indicador de carga al obtener los datos
+    setIsLoading(true);
     const dataCloudFirestore = await handleGetData();
     const dataSurvey = dataCloudFirestore.filter(
-      data => data.dataType === 'written',
+      (data) => data.dataType === 'written'
     );
-    const datos = [];
-    dataSurvey.map((data, i) => {
-      datos.push(data.results);
-    });
+    const datos = dataSurvey.map((data) => data.results);
     procesarRespuestas(datos);
   };
 
-  const procesarRespuestas = async respuestas => {
+  const procesarRespuestas = async (respuestas) => {
     if (respuestas && respuestas.length >= 1) {
-      const preguntasAProcesar = preguntas[0].questions; // Obtén las preguntas del arreglo preguntas
+      const preguntasAProcesar = preguntas[0].questions;
       const dataProcesada = [];
 
       for (const pregunta of preguntasAProcesar) {
         if (pregunta.questionType === 'numberInput') {
-            const respuestasPregunta = {};
-            const intervalos = [0, 10, 20, 30, 40]; // Define los intervalos según tu lógica
-    
-            respuestas.forEach(respuesta => {
-              const valorRespuesta = respuesta[pregunta.questionID];
-              for (let i = 0; i < intervalos.length - 1; i++) {
-                if (valorRespuesta >= intervalos[i] && valorRespuesta < intervalos[i + 1]) {
-                  const rangoIntervalo = `${intervalos[i]}-${intervalos[i + 1]}`;
-                  if (!respuestasPregunta.hasOwnProperty(rangoIntervalo)) {
-                    respuestasPregunta[rangoIntervalo] = 0;
-                  }
-                  respuestasPregunta[rangoIntervalo]++;
-                  break;
+          const respuestasPregunta = {};
+          const intervalos = [0, 10, 20, 30, 40];
+
+          respuestas.forEach((respuesta) => {
+            const valorRespuesta = respuesta[pregunta.questionID];
+            for (let i = 0; i < intervalos.length - 1; i++) {
+              if (
+                valorRespuesta >= intervalos[i] &&
+                valorRespuesta < intervalos[i + 1]
+              ) {
+                const rangoIntervalo = `${intervalos[i]}-${intervalos[i + 1]}`;
+                if (!respuestasPregunta.hasOwnProperty(rangoIntervalo)) {
+                  respuestasPregunta[rangoIntervalo] = 0;
                 }
-              
+                respuestasPregunta[rangoIntervalo]++;
+                break;
+              }
             }
           });
+
           dataProcesada.push({
             pregunta: pregunta.title,
             respuestas: respuestasPregunta,
           });
         } else if (pregunta.questionType === 'radioButton') {
-          // Procesar preguntas con opciones de selección (respuestas múltiples)
           const respuestasPregunta = {};
 
-          // Inicializa el objeto con las respuestas posibles para esta pregunta
-          pregunta.options.forEach(opcion => {
+          pregunta.options.forEach((opcion) => {
             respuestasPregunta[opcion] = 0;
           });
 
-          // Itera sobre las respuestas y cuenta cuántas personas seleccionaron cada opción
-          respuestas.forEach(respuesta => {
+          respuestas.forEach((respuesta) => {
             const respuestaPregunta = respuesta[pregunta.questionID];
             if (respuestasPregunta.hasOwnProperty(respuestaPregunta)) {
               respuestasPregunta[respuestaPregunta]++;
             }
           });
 
-          // Agrega los datos procesados al array dataProcesada
           dataProcesada.push({
             pregunta: pregunta.title,
             respuestas: respuestasPregunta,
@@ -88,35 +79,32 @@ export const GraficaScreen = () => {
     }
   };
 
-  const generarDatosGrafica = indicePregunta => {
+  const generarDatosGrafica = (indicePregunta) => {
     const pregunta = dataGraficas[indicePregunta];
     const respuestas = pregunta.respuestas;
-    const labels = Object.keys(respuestas).map(respuesta => respuesta); // Utiliza las respuestas como etiquetas
+    const labels = Object.keys(respuestas).map((respuesta) => respuesta);
     const valores = Object.values(respuestas);
-    // console.log(labels)
+
     return {
-      dataSets: [
+      labels: labels,
+      datasets: [
         {
-          values: valores,
-          label: 'Respuestas',
-          config: {
-            colors: [processColor('#2979FF')], // Color de las barras
-            valueTextSize: 12,
-          },
+          data: valores,
+          color: (opacity = 1) => `rgba(41, 121, 255, ${opacity})`,
+          barPercentage: 0.8,
         },
       ],
-      labels: labels,
     };
   };
 
   return (
-    <ScrollView style={{flex: 1}}>
-      <View style={{flex: 1}}>
+    <ScrollView style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
         {isLoading ? (
           <ActivityIndicator size="large" color="#2979FF" />
         ) : (
           dataGraficas.map((pregunta, index) => (
-            <View key={index} style={{height: 300}}>
+            <View key={index} style={{ height: 300 }}>
               <Text
                 style={{
                   textAlign: 'center',
@@ -126,13 +114,20 @@ export const GraficaScreen = () => {
                 {pregunta.pregunta}
               </Text>
               <BarChart
-                style={{flex: 1}}
                 data={generarDatosGrafica(index)}
-                xAxis={{drawLabels: true, position: 'BOTTOM'}}
-                yAxisRight={{drawLabels: false}}
-                yAxisLeft={{drawLabels: true}}
-                chartDescription={{text: ''}}
-                legend={{enabled: true}}
+                width={Dimensions.get('window').width}
+                height={220}
+                yAxisLabel=""
+                chartConfig={{
+                  backgroundGradientFrom: '#ffffff',
+                  backgroundGradientTo: '#ffffff',
+                  decimalPlaces: 0,
+                  color: (opacity = 1) => `rgba(41, 121, 255, ${opacity})`,
+                  labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+                  style: {
+                    borderRadius: 16,
+                  },
+                }}
               />
             </View>
           ))
